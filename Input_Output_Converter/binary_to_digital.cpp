ad80b4155550723af6cd469024310dacdd654536
kopricky <kopricky@yahoo.co.jp>
2018-07-21 07:50:55 +0900

    revise

--- Input_Output_Converter/binary_to_digital.cpp
+++ Input_Output_Converter/binary_to_digital.cpp
@@ -1,3 +1,5 @@
+//charで受け取っているためbinaryの長さがバイト単位ではなく躊躇半端に切れている場合返り値の最後の数桁に余分な0が加わる
+
 #include <iostream>
 #include <fstream>
 #include <cassert>
@@ -5,13 +7,6 @@
 #define ll long long
 
 using namespace std;
-
-struct coordinate
-{
-    int x,y,z;
-    coordinate(){}
-    coordinate(int x_, int y_, int z_) : x(x_), y(y_), z(z_){}
-};
 
 int mdl_to_txt()
 {
@@ -26,42 +21,15 @@ int mdl_to_txt()
         return 1;
     }
 
-    char buff;
-
-    fin.read(&buff, sizeof(char));
-
-    int R = 0;
-
-    for(int i = 0; i < 8; i++){
-        if((buff >> i)&1) R += (1 << i);
-    }
-
-    fout << R << "\n";
-
-    int cube = R * R * R;
-
-    int index = 0;
-
-    vector<coordinate> block;
-
     while(!fin.eof()) {
         char buff;
         fin.read(&buff,sizeof(char));
-        for(int i = 0; i < 8 && index < cube; i++, index++){
-            if((buff >> i)&1){
-                block.emplace_back(index / R / R, (index / R) % R, index % R);
-            }
+        for(int i = 0; i < 8; i++){
+            fout << (buff >> i);
         }
     }
 
-    fout << (int)block.size() << "\n";
-
-    for(auto& val : block){
-        fout << val.x << " " << val.y << " " << val.z << "\n";
-    }
-
-    fin.close();
-    fout.close();
+    cout << "\n";
 
     return 0;
 }
