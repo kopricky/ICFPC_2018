78d232eee4388b9ac68b4bf90995cb0efbdde375
kopricky <kopricky@yahoo.co.jp>
2018-07-21 22:53:38 +0900

    出力の変換について

--- Output/output_converter.cpp
+++ Output/output_converter.cpp
@@ -21,9 +21,9 @@ pair<vector<int>, vector<int> > short_move(string dir, int dist)
 {
     int a;
     if(dir == "x"){
-        a = 2;
-    }else if(dir =="y"){
         a = 1;
+    }else if(dir =="y"){
+        a = 2;
     }else if(dir == "z"){
         a = 3;
     }else{
@@ -36,9 +36,9 @@ pair<vector<int>, vector<int> > long_move(string dir, int dist)
 {
     int a;
     if(dir == "x"){
-        a = 2;
-    }else if(dir =="y"){
         a = 1;
+    }else if(dir =="y"){
+        a = 2;
     }else if(dir == "z"){
         a = 3;
     }else{
@@ -72,22 +72,22 @@ int txt_to_nbt()
             fout.write((char*) &num, sizeof(char));
             return 0;
         }else if(s == "Wait"){
-            int num = (1 << 7) - 1;
+            int num = (1 << 8) - 2;
             fout.write((char*) &num, sizeof(char));
         }else if(s == "Flip"){
-            int num = (1 << 8) - (1 << 6) - 1;
+            int num = (1 << 8) - 3;
             fout.write((char*) &num, sizeof(char));
         }else if(s == "SMove"){
             string dir;
             int dist;
             fin >> dir >> dist;
-            int num1 = (1 << 5), num2 = 0;
+            int num1 = (1 << 2), num2 = 0;
             auto p = long_move(dir, dist);
             for(int i = 0; i < (int)(p.first).size(); i++){
-                num1 += (p.first[i] << (i+2));
+                num1 += (p.first[i] << (i+4));
             }
             for(int i = 0; i < (int)(p.second).size(); i++){
-                num2 += (p.second[i] << (i+3));
+                num2 += (p.second[i] << i);
             }
             fout.write((char*) &num1, sizeof(char));
             fout.write((char*) &num2, sizeof(char));
@@ -97,28 +97,28 @@ int txt_to_nbt()
             fin >> dir1 >> dist1 >> dir2 >> dist2;
             auto p1 = short_move(dir1, dist1);
             auto p2 = short_move(dir2, dist2);
-            int num1 = (1 << 4) + (1 << 5), num2 = 0;
-            for(int i = 0; i < (int)(p2.first).size(); i++){
-                num1 += (p2.first[i] << i);
-            }
+            int num1 = (1 << 2) + (1 << 3), num2 = 0;
             for(int i = 0; i < (int)(p1.first).size(); i++){
-                num1 += (p1.first[i] << (i+2));
+                num1 += (p1.first[i] << (i+4));
             }
-            for(int i = 0; i < (int)(p2.second).size(); i++){
-                num2 += (p2.second[i] << i);
+            for(int i = 0; i < (int)(p2.first).size(); i++){
+                num1 += (p2.first[i] << (i+6));
             }
             for(int i = 0; i < (int)(p1.second).size(); i++){
-                num2 += (p1.second[i] << (i+4));
+                num2 += (p1.second[i] << i);
+            }
+            for(int i = 0; i < (int)(p2.second).size(); i++){
+                num2 += (p2.second[i] << (i+4));
             }
             fout.write((char*) &num1, sizeof(char));
             fout.write((char*) &num2, sizeof(char));
         }else if(s == "Fission"){
             int dx, dy, dz, m;
             fin >> dx >> dy >> dz >> m;
-            int num1 = (1 << 5) + (1 << 7), num2 = 0;
+            int num1 = 1 + (1 << 2), num2 = 0;
             auto p1 = near_move(dx, dy, dz);
             for(int i = 0; i < (int)p1.size(); i++){
-                num1 += (p1[i] << i);
+                num1 += (p1[i] << (i+3));
             }
             auto p2 = convert_to_zero_one(m, 8);
             for(int i = 0; i < (int)p2.size(); i++){
@@ -129,28 +129,28 @@ int txt_to_nbt()
         }else if(s == "Fill"){
             int dx, dy, dz;
             fin >> dx >> dy >> dz;
-            int num = (1 << 6) + (1 << 7);
+            int num = 3;
             auto p = near_move(dx, dy, dz);
             for(int i = 0; i < (int)p.size(); i++){
-                num += (p[i] << i);
+                num += (p[i] << (i+3));
             }
             fout.write((char*) &num, sizeof(char));
         }else if(s == "FusionP"){
             int dx, dy, dz;
             fin >> dx >> dy >> dz;
-            int num = (1 << 5) + (1 << 6) + (1 << 7);
+            int num = 7;
             auto p = near_move(dx, dy, dz);
             for(int i = 0; i < (int)p.size(); i++){
-                num += (p[i] << i);
+                num += (p[i] << (i+3));
             }
             fout.write((char*) &num, sizeof(char));
         }else if(s == "FusionS"){
             int dx, dy, dz;
             fin >> dx >> dy >> dz;
-            int num = (1 << 5) + (1 << 6);
+            int num = (1 << 1) + (1 << 2);
             auto p = near_move(dx, dy, dz);
             for(int i = 0; i < (int)p.size(); i++){
-                num += (p[i] << i);
+                num += (p[i] << (i+3));
             }
             fout.write((char*) &num, sizeof(char));
         }else{
